4515b9b725eb88a745e3f9f166dedc5d2a91c7e3
shoaib patel <100205704+pshoaib@users.noreply.github.com>
2022-06-29 20:30:56 +0530

    Import
    Import_Export

---
+++ Class_Import_Export_Self/scripts/index.js
@@ -0,0 +1,13 @@
+
+
+let navbar = () =>
+{
+    return `  
+    <div id="nav-div">
+            <h2> <a href="/jwelary.html">jwelary</a></h2>
+           <h2><a href="/jwelary.html">Electronics</a></h2>
+           <h2><a href="/index.html">Log In</a></h2>
+           <h2><a href="/index.html">Home</a></h2>
+   </div>`
+}
+export default navbar();
